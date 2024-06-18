1b22a179d25eb09e5a0b3bf05dea75f42796e681
Luna Simons <luna@bddvlpr.com>
2024-06-18 17:29:00 +0200

    fix(drizzle): provider unique constraint

--- src/lib/server/database/schema/users.ts
+++ src/lib/server/database/schema/users.ts
@@ -1,11 +1,17 @@
-import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, serial, timestamp, unique, varchar } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users', {
-    id: serial('id').primaryKey(),
+export const users = pgTable(
+    'users',
+    {
+        id: serial('id').primaryKey(),
 
-    provider: varchar('provider', { length: 256 }).notNull(),
-    providerId: varchar('providerId', { length: 256 }).notNull(),
+        provider: varchar('provider', { length: 256 }).notNull(),
+        providerId: varchar('providerId', { length: 256 }).notNull(),
 
-    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
-    updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date())
-});
+        createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+        updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date())
+    },
+    ({ provider, providerId }) => ({
+        provider: unique().on(provider, providerId)
+    })
+);
